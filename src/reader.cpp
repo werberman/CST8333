7042ec558b7adfd611edf7e6b6d47d5e027f436a
werberman <multimusicman@gmail.com>
2022-01-19 13:50:05 -0500

    Reader is finally fully fixed

--- src/reader.cpp
+++ src/reader.cpp
@@ -33,6 +33,8 @@
 
 using namespace std;
 
+string ReplaceAll(string str, const string& from, const string& to);
+
 /**
  * @brief
  *
@@ -88,21 +90,10 @@ Data_Bundle reader(string fname)
             stringstream str(line);
 
             while (getline(str, word, ','))
-                row.push_back(word);
+                row.push_back(ReplaceAll(string(word), string("*"), string(",")));
                 content.push_back(row);
         }
-        // Change the stars back to commas
-        // for (int i = 0; i < content.size(); i++)
-        // {
-        //     cout << content[0].size();
-        //     cout << content.size();
-        //     cout << "\nTEMP";
-        //     for (int j = 0; j < content[0].size(); i++)
-        //     {
-        //         cout << "BLAH";
-        //         // content[j][i].replace(content[j][i].begin(), content[j][i].end(), '*', ',');
-        //     }
-        // };
+
     }
     else
         cout << "Could not open the file\n";
@@ -130,4 +121,25 @@ Data_Bundle reader(string fname)
     bundle.row_keys = incidentNumbers;
 
     return bundle;
+};
+
+/**
+ * @brief 
+ * Replace all given substrings of a string with another substring
+ * https://stackoverflow.com/questions/2896600/how-to-replace-all-occurrences-of-a-character-in-string
+ * 
+ * @param str string to be changed
+ * @param from substring to be changed
+ * @param to new substring
+ * @return std::string 
+ */
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to)
+{
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != string::npos)
+    {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+    return str;
 }
