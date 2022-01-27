10cc2b7b6717ffcfe5e9acbd2d308207c7c7cb8f
werberman <multimusicman@gmail.com>
2022-01-27 15:39:51 -0500

    Changed to 3rd party csv parser

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,6 @@
 #define DARTA_CPP
 #include "Data.cpp"
 #endif
-#ifndef READER_CPP
-#define READER_CPP
-#include "reader.cpp"
-#endif
 #ifndef IOSTREAM
 #define IOSTREAM
 #include <iostream>
@@ -30,39 +26,55 @@
 #define EXCEPTION
 #include <exception>
 #endif
+#ifndef READER_CPP
+#define READER_CPP
+#include "reader.cpp"
+#endif
 
 using namespace std;
 
 /**
- * @brief Load the given file and ask the user how many records they wish to display. Only 
- * displays the first 5 fields of each record. 
- * 
+ * @brief Load the given file and ask the user how many records they wish to display. Only
+ * displays the first 5 fields of each record.
+ *
  * @return int 0 when finished.
  */
 int main()
 {
     int m;
     string fname = "C:/Sjunk/C++/pipeline-incidents-comprehensive-data.csv";
-
     Data_Bundle bundle;
 
-try {
-    bundle = reader(fname);
-} catch (Read_Exception& e1 )
-{
-    cout << "Could not open file\n";
-    return 0;
-}
-    
+    try
+    {
+        bundle = reader(bundle, fname);
+    }
+    catch (Read_Exception &e1)
+    {
+        cout << "Could not open the file!";
+        return 0;
+    }
+
+    // try
+    // {
+    //     bundle = reader(fname);
+    // }
+    // catch (Read_Exception &e1)
+    // {
+    //     cout << "Could not open file\n";
+    //     return 0;
+    // }
+
     cout << "Select the number of records to display: ";
     cin >> m;
     cout << "\n";
     for (int i = 0; i < 5; i++)
     {
-        cout << "*******************************\n" << bundle.data_headers.getColumn_headers()[i] << " : \n";
+        cout << "*******************************\n"
+             << bundle.data_headers.getColumn_headers()[i] << " : \n";
         for (int j = 0; j < m; j++)
         {
-            cout << "Record No: " << (j+1) << ": " << bundle.data_rows.getColumn_data()[j][i] << endl;
+            cout << "Record No: " << (j + 1) << ": " << bundle.data_rows.getColumn_data()[j][i] << endl;
         }
     }
 
